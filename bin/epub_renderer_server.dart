220e2913e31f1096b76743dfe9530e49af240f6e
FlafyDev <flafyarazi@gmail.com>
2022-07-25 05:19:35 +0300

    new path

--- bin/epub_renderer_server.dart
+++ bin/epub_renderer_server.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
 import 'package:epub_renderer_server/epub_renderer_server.dart';
 
 void main(List<String> arguments) async {
   final server = EpubRendererServer(
-    port: 8081,
+    port: 8082,
+    epubRendererHtml:
+        File("/mnt/general/repos/flafydev/epub-renderer/dist/index.html"),
+    // epubResourcesPath:
+    //     "/home/flafydev/Documents/book_reader/mybook2/extracted/EPUB",
     epubResourcesPath:
-        "/home/flafydev/Documents/book_reader/mybook2/extracted/EPUB",
+        "/home/flafydev/Documents/book_reader/mybook/extracted/OEBPS/",
   );
 
   await server.start();
--- lib/epub_renderer_server.dart
+++ lib/epub_renderer_server.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:path/path.dart' as p;
 import 'package:mime/mime.dart';
@@ -5,11 +6,13 @@ import 'package:mime/mime.dart';
 class EpubRendererServer {
   final int port;
   final String epubResourcesPath;
+  final File epubRendererHtml;
   HttpServer? server;
 
   EpubRendererServer({
     required this.port,
     required this.epubResourcesPath,
+    required this.epubRendererHtml,
   });
 
   Future<void> start() async {
@@ -29,46 +32,41 @@ class EpubRendererServer {
     final pathSplit =
         path.split("/").where((element) => element.isNotEmpty).toList();
 
-    request.response.headers.add('Access-Control-Allow-Origin', '*');
-
-    switch (pathSplit[0]) {
-      case "epub":
-        {
-          final filePath = pathSplit.skip(1).join("/");
-
-          File file = File(p.join(epubResourcesPath, filePath));
-
-          if (!await file.exists()) {
-            request.response
-              ..statusCode = HttpStatus.notFound
-              ..close();
-            return;
-          }
-
-          final List<int> content;
-          final mimeType = lookupMimeType(file.path);
-
-          if (mimeType == null) {
-            request.response
-              ..statusCode = HttpStatus.badRequest
-              ..close();
-            return;
-          }
-
-          request.response
-            ..headers.contentType = ContentType.parse(mimeType)
-            ..add(file.readAsBytesSync())
-            ..close();
-
-          return;
-        }
-      default:
-        {
-          request.response
-            ..statusCode = HttpStatus.notFound
-            ..close();
-          return;
-        }
+    if (pathSplit.isEmpty) {
+      final htmlContent = await epubRendererHtml.readAsBytes();
+      request.response
+        ..headers.contentType = ContentType.html
+        ..add(htmlContent)
+        ..close();
+      return;
     }
+
+    final filePath = pathSplit.join("/");
+
+    File file = File(p.join(epubResourcesPath, filePath));
+
+    if (!await file.exists()) {
+      request.response
+        ..statusCode = HttpStatus.notFound
+        ..close();
+      return;
+    }
+
+    final List<int> content;
+    final mimeType = lookupMimeType(file.path);
+
+    if (mimeType == null) {
+      request.response
+        ..statusCode = HttpStatus.badRequest
+        ..close();
+      return;
+    }
+
+    request.response
+      ..headers.contentType = ContentType.parse(mimeType)
+      ..add(file.readAsBytesSync())
+      ..close();
+
+    return;
   }
 }
